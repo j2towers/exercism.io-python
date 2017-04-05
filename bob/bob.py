4611bf292a0cd335a2afd509ed2e2b133981ffa6
Jeff Towers <j2towers@uwaterloo.ca>
2017-04-05 02:30:01 -0400

    Bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,10 @@
-def hey():
-    pass
+def hey(saidtobob):
+        saidtobob = saidtobob.rstrip()
+    if saidtobob.isupper():
+        return "Whoa, chill out!"
+    elif saidtobob.endswith("?"):
+        return "Sure."
+    elif saidtobob.isspace() or saidtobob == "":
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
