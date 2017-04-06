43fc3b758263d7b8b640dabe26d288643bb470b2
Jeff Towers <j2towers@uwaterloo.ca>
2017-04-06 01:09:46 -0400

    Rotational Cipher

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,26 @@
-def rotate():
-    pass
+import string
+
+def rotate(stringin, rotation):
+    outstring = ""
+
+    for charitem in stringin:
+        if charitem.isalpha():
+            if charitem.isupper():
+                for index, letter in enumerate(string.ascii_uppercase):
+                    if charitem is letter:
+                        if index + rotation < len(string.ascii_uppercase) - 1:
+                            outstring += string.ascii_uppercase[index + rotation]
+                        else:
+                            newdex = (index + rotation) - 26
+                            outstring += string.ascii_uppercase[newdex]
+            elif charitem.islower():
+                for index, letter in enumerate(string.ascii_lowercase):
+                    if charitem is letter:
+                        if index + rotation < len(string.ascii_lowercase) - 1:
+                            outstring += string.ascii_lowercase[index + rotation]
+                        else:
+                            newdex = (index + rotation) - 26
+                            outstring += string.ascii_lowercase[newdex]
+        else:
+            outstring += charitem
+    return outstring
