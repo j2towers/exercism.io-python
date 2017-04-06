cc4dd57d4a384f32cd78126372d3ab9beebf054a
Jeff Towers <j2towers@uwaterloo.ca>
2017-04-06 14:35:21 -0400

    Anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,11 @@
-def detect_anagrams():
-    pass
+def detect_anagrams(stringin, listin):
+    listout = list()
+
+    for a in listin:
+        astring = str(a).lower()
+        instring = str(stringin).lower()
+        if instring != astring:
+            if sorted(instring) == sorted(astring):
+                listout.append(a)
+
+    return listout
