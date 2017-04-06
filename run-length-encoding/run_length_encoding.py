b0eed4e67663e934a67b10c047f307d8d33b9951
Jeff Towers <j2towers@uwaterloo.ca>
2017-04-05 21:14:10 -0400

    Run Length Encoding
    fails 3 encode tests because it encodes single chars with a 1 in front

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,41 +1,11 @@
 
+from re import sub
+
 def decode(text):
-    mult = 1
-    textout = ""
-    for index, char in enumerate(text):
-        if char.isdigit():
-            if text[index - 1].isdigit():
-                mult = int(str(text[index - 1] + char))
-            else:
-                mult = char
-        elif char.isalpha() or char.isspace():
-            if text[index - 1].isalpha() or text[index - 1].isspace():
-                textout += char
-            else:
-                for run in range(int(mult)):
-                    textout += char
-    return textout
-
-
+    return sub(r'(\d+)(\D)', lambda m: m.group(2) * int(m.group(1)),
+               text)
 
 def encode(text):
-    textout = ""
-    count = 1
-    maxidx = len(text)
-    for index, char in enumerate(text):
-        if index < maxidx - 1:
-            if char == text[index + 1]:
-                count += 1
-            else:
-                if count > 1:
-                    textout += str(count) + char
-                    count = 1
-                else:
-                    textout += char
-        else:
-            if count > 1:
-                textout += str(count) + char
-            else:
-                textout += char
-    return textout
+    return sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1),
+               text)
 
