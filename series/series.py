69756bad11d0643b692e0069ad29ce536917a1da
Jeff Towers <j2towers@uwaterloo.ca>
2017-04-07 03:18:14 -0400

    Series

--- series/series.py
+++ series/series.py
@@ -1,2 +1,16 @@
-def slices():
-    pass
+def slices(stringin, slength):
+
+    listout = list()
+
+    if slength > len(stringin) or slength == 0:
+        raise ValueError
+    else:
+        for index, num in enumerate(stringin):
+            liststring = list()
+            if index + slength <= len(stringin):
+                for a in range(slength):
+                    liststring.append(int(stringin[index + a]))
+                listout.append(liststring)
+        return listout
+
+
