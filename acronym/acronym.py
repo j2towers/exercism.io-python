2808d1d8114c23a9c02c7f518ea71a20383aa148
Jeff Towers <j2towers@uwaterloo.ca>
2017-04-11 00:27:50 -0400

    Acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,17 @@
-def abbreviate():
-    pass
+
+
+def abbreviate(stringin):
+    stringout = ""
+    listin = list()
+
+    if ":" in stringin:
+        listin = stringin.split(":")
+        return listin[0].strip()
+    else:
+        for index, testitem in enumerate(stringin):
+            if testitem.isupper() or stringin[index - 1].isspace() or not stringin[index - 1].isalnum():
+                print(testitem)
+                if stringin[index - 1].isspace() or stringin[index - 1].islower() or not stringin[index - 1].isalnum():
+                    stringout += testitem.strip()
+        return stringout.upper()
+
