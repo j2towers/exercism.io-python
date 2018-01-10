676348dd191b9866a00e695251a9b2a7fd2e5ba5
Jeff Towers <j2towers@uwaterloo.ca>
2018-01-09 22:06:56 -0500

    largest series

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,43 @@
+def largest_product(series, size):
+
+    total = 0
+
+    if (not str.isalpha(series) and len(series) >= 0):
+
+        if (size >= 0 and size <= len(series)):
+
+            for digit in range(len(series)):
+
+                addarray = []
+                subtotal = 1
+
+                for arraybuild in range(size):
+
+                    if (digit + arraybuild < len(series)):
+                        addarray.append(int(series[digit + arraybuild]))
+
+                if (len(addarray) == size):
+                    for addnum in addarray:
+
+                        subtotal *= addnum
+
+                    if (subtotal > total):
+
+                        total = subtotal
+
+        else:
+
+            raise ValueError("Size error.")
+
+    else:
+
+        raise ValueError("Series is not a number.")
+
+    return total
+
+
+
+
+
+
+
